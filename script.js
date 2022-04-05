02d2b0cbb320b81c340b360350e37c12eb4d566d
Sarka Sevcikova <sarka.sevci@gmail.com>
2022-04-05 21:40:45 +0200

    fonts, updated cv

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+// REVIEWS
+
 const reviews = [
     {
         id: 1,
         name: "KAREL HASEK",
         job: "former direct manager",
         text: "Sarka has excellent people skills and is able to work seamlessly with many different roles both internally and externally, her technical knowledge is also of the highest level. She’s extremely fast learner. Sarka has very deep knowledge of web standards, technologies and great understanding of topics such as SEO, Accessibility and Web Analytics. I can wholeheartedly recommend Sarka."
-    },
+    },   
     {
         id: 2,
         name: "CRISTINA IORDAN",
-        job: "direct manager",
+        job: "former direct manager",
         text: "A pleasure to recommend Sarka as a very bright and motivated person that she is. A quick learner, which has shown the ability to work on multiple projects, prioritizing the marketing requests in line with business priorities. Sarka is remarkable through high quality output delivery, on time, in budget and within scope."
     },
     {
         id: 3,
+        name: "IRINA VERLAN",
+        job: "former colleague",
+        text: "Sarka is the most professional and efficient at work, as well as well-presented and self-motivated, and she can work both independently and as part of a team. I recommend her because of her attention to detail and teamwork spirit. Sarka has proven to be a trustworthy and productive team member. I'm confident she'll be an excellent addition to any team she joins."
+    },
+    {
+        id: 4,
         name: "JURAJ KOVAC",
         job: "mentor",
         text: "If you are looking for open-minded talent and are willing to offer some support to a capable learner, I am sure Sarka will be a valuable contribution to any creative project."
